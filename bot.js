16e7b3b81cd61aa07da1797af81d7ccd449ed67b
sunray4 <118241363+sunray4@users.noreply.github.com>
2024-08-18 16:56:52 -0700

    fix bug in finding next fiducial user and locating general channel

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ client.on('guildCreate', async (guild) => {
     // Check if the welcome message has already been sent
     if (hasSentWelcomeMessage) return;
 
-    const generalChannel = guild.channels.cache.find(channel => channel.name === 'general' && channel.type === 'GUILD_TEXT');
+    const generalChannel = guild.channels.cache.find(channel => channel.name === 'general');
     if (generalChannel) {
         // Send a welcome message to the general channel
         await generalChannel.send("hey guys im AmpFire, your custom discord bot!! i can moderate fiducial if prompted by '/fiducial' in the fiducial channel through tagging the next fiducial number in the #kindergarten channel. ill skip to the next fiducial number if there is no response in 48 hours. hopefully ill get more features soon... ");
@@ -48,7 +48,7 @@ client.on('messageCreate', async (message)=> {
         for (let i = 1; i <= 52; i++) {
             console.log("Finding user...");
             //find user with the next fiducial number
-            const user = message.guild.members.cache.find(member => member.nickname && member.nickname.includes(i.toString()) && !member.nickname.includes("1" + i.toString()) && !member.nickname.includes("2" + i.toString()) && !member.nickname.includes("3" + i.toString()) && !member.nickname.includes("4" + i.toString()) && !member.nickname.includes("5" + i.toString()));        
+            const user = message.guild.members.cache.find(member => member.nickname && member.nickname.includes(i.toString() +  " ") && member.nickname.indexOf(i.toString()) == 0);        
             if (user) {
                 console.log("Found user. Waiting for message");
 
@@ -90,10 +90,10 @@ client.on('messageCreate', async (message)=> {
             await fiducialChannel.send("well done for completing fiducial!! everyone is present congratulations :D");
         }
         else if (absent == 1) {
-            await fiducialChannel.send(`Well done for completing fiducial!! only ${absent} person is absent in this round `);
+            await fiducialChannel.send(`well done for completing fiducial!! only ${absent} person is absent in this round `);
         }
         else {
-            await fiducialChannel.send(`Well done for completing fiducial!! ${absent} people are absent in this round`);
+            await fiducialChannel.send(`well done for completing fiducial!! ${absent} people are absent in this round`);
         }
 
     }
