7e9d23b5bf4d49b18b78bcb7848fc6143300f339
sunray4 <118241363+sunray4@users.noreply.github.com>
2024-08-18 14:59:50 -0700

    Time delay error debug and add fiducial phrase generator

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import {Client, GatewayIntentBits} from 'discord.js';
-import { paGenerator } from './paGenerator.js';
+import { paGenerator, fiducialPhraseGenerator } from './word-generator.js';
 
 const client = new Client({
     intents:  [
@@ -27,19 +27,23 @@ client.on('messageCreate', async (message)=> {
     if (message.channel.id === fiducialChannel.id && message.content === '/fiducial') {
         var absent = 0;
         for (let i = 1; i <= 52; i++) {
+            console.log("Finding user...");
             const user = message.guild.members.cache.find(member => member.nickname && member.nickname.includes(i.toString()));        
             if (user) {
-                await kindergartenChannel.send(`${user} you're up for fiducial!!`);
+                await kindergartenChannel.send(`${user} ${fiducialPhraseGenerator()}`);
                 const filter = (msg) => msg.content.includes(i.toString()) && msg.author.id === user.id && msg.channel === fiducialChannel.id;
                 
-                // setTimeout(() => {
-                //     kindergartenChannel.send(`${user} 36 hours is a long time to keep us waiting...`);
-                // }, 10000);
+                setTimeout(() => {
+                    kindergartenChannel.send(`${user} 36 hours is a long time to keep us waiting...`);
+                }, 10000);
                 await fiducialChannel.awaitMessages({
                     filter,
                     max: 1,
                     time: 100000,
                     error: ['time'],
+                })
+                .then(collected => {
+                    console.log(`Message received: ${collected.first().content}`);
                 })
                 .catch(() => {
                     fiducialChannel.send(`\"${i}\" - ${paGenerator()}`);
@@ -49,7 +53,8 @@ client.on('messageCreate', async (message)=> {
                 
             }
             else {
-                await fiducialChannel.send(`\"${i} \"- ${paGenerator()}`);
+                console.log("User cannot be found");
+                await fiducialChannel.send(`\"${i}\" - ${paGenerator()}`);
                 absent++;
             }
         }
--- paGenerator.js
+++
@@ -1,7 +0,0 @@
-export function paGenerator () {
-    var pas = ["Rajan", "Patricia", "Beck", "Abbie", "Ally", "Priyanka", "Sal", "Kie", "Erin", "Josh", "Kyle"];
-
-    var pa = pas[Math.floor(Math.random() * pas.length)];
-    return pa;
-}
-
