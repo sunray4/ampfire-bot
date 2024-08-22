8a98883a5418c5020ed2ea622c79d212b81bef33
sunray4 <118241363+sunray4@users.noreply.github.com>
2024-08-21 20:35:20 -0700

    Change default port to 10000

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ app.get('/', (req, res) => {
   res.send('Bot is alive!');
 });
 
-const PORT = process.env.PORT || 3000;
+//10000 is the default port of render
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
